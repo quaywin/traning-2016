9c6bfad7db7dcb8ee7c4aff9c80c7f9727270829
Hồ Ngô Thanh Khôi <thanhkhoi95@gmail.com>
2017-03-30 10:59:22 +0700

    commit item model

---
+++ shopProject/models/item.model.js
@@ -0,0 +1,30 @@
+var mongoose = require('mongoose');
+var schema = mongoose.Schema;
+
+var itemSchema = schema({
+    name: {
+        required: true,
+        type: String
+    },
+    price: {
+        required: true,
+        type: Number
+    },
+    image: [String],
+    provider: {
+        required: true,
+        type: String
+    },
+    amount: {
+        required: true,
+        type: Number
+    },
+    category: {
+        required: true,
+        type: String
+    }
+});
+
+var item = new mongoose.model('item', itemSchema);
+
+modules.exports = item;
