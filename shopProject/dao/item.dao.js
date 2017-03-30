c7ea018a86b334d1fe774f580ff317445e2bab5f
Hồ Ngô Thanh Khôi <thanhkhoi95@gmail.com>
2017-03-30 11:57:56 +0700

    add delete function in item dao

--- shopProject/dao/item.dao.js
+++ shopProject/dao/item.dao.js
@@ -1,12 +1,36 @@
 var Item = require('../models/item.model');
 
 module.exports = {
-    createItem: createItem
+    createItem: createItem,
+    updateItem: updateItem,
+    deleteItem: deleteItem
 }
 
 function createItem(itemReq, callback) {
     var newItem = new Item();
+    newItem.name = itemReq.name;
+    newItem.price = itemReq.price;
+    newItem.image = itemReq.image;
+    newItem.provider = itemReq.provider;
+    newItem.amount = itemReq.amount;
+    newItem.category = itemReq.category;
 
+    newItem.save(function(err) {
+        if (err) callback(err);
+        else
+            callback(null, {
+                mess: "create successfully"
+            })
+    })
+}
 
-    newItem.save()
+function deleteItem(itemID, callback) {
+    Item.remove( {_id: itemID}, function(err, reponse){
+        if (err) callback(err);
+        else {
+            callback(null,{
+                "Message": "Deleted"
+            });
+        }
+    });
 }
